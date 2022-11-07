6ef308459fd05b9a56243db41fa70cb27d1342d1
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-07 16:04:00 +0100

    [add] milestone_2 complete

--- milestone_2/assets/js/app.js
+++ milestone_2/assets/js/app.js
@@ -12,21 +12,21 @@ createApp({
                 avatar: '_1',
                 visible: true,
                 messages: [
-                {
-                date: '10/01/2020 15:30:55',
-                message: 'Hai portato a spasso il cane?',
-                status: 'sent'
-                },
-                {
-                date: '10/01/2020 15:50:00',
-                message: 'Ricordati di stendere i panni',
-                status: 'sent'
-                },
-                {
-                date: '10/01/2020 16:15:22',
-                message: 'Tutto fatto!',
-                status: 'received'
-                }
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
                 ],
                 },
                 {
@@ -177,8 +177,7 @@ createApp({
             this.activeIndex = index;
             console.log(index);
             console.log(this.activeIndex);
-        }
-
+        },
     },
     created(){
 //        console.log('creata');
